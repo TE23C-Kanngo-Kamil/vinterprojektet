062ed80fbff301ecce5cd4f36a440b0c531089b0
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2025-01-17 14:29:30 +0100

    Red Light, Green Light

--- vinterprojektet/Program.cs
+++ vinterprojektet/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 800, "Red Light, Green Light");
+Raylib.SetTargetFPS(60);
+
+while (Raylib.WindowShouldClose() == false)
+{
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.Beige);
+    Raylib.DrawLine(800, 100, 0, 100, Color.Pink);
+
+    Raylib.EndDrawing();
+}
