e99f639c830b424fda3bf74ca7b1395c5c9170a3
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2025-01-30 13:04:55 +0100

    Day 5

--- vinterprojektet/Program.cs
+++ vinterprojektet/Program.cs
@@ -1,10 +1,22 @@
 ﻿using System.Numerics;
 using Raylib_cs;
 
+// Skärmdimensioner
 const int screenWidth = 800;
-const int screenHeight = 800;
+const int screenHeight = 1000;
+
 Raylib.InitWindow(screenWidth, screenHeight, "Red Light, Green Light");
 Raylib.SetTargetFPS(60);
+
+// Karaktärens egenskaper
+float circleRadius = 20;
+float rectangleWidth = 40;
+float rectangleHeight = 60;
+float speed = 2f;
+
+// Börja längst ner
+float circleX = screenWidth / 2f;
+float circleY = screenHeight - rectangleHeight - circleRadius;
 
 while (Raylib.WindowShouldClose() == false)
 {
@@ -14,39 +26,34 @@ while (Raylib.WindowShouldClose() == false)
     Raylib.DrawRectangle(0, 125, 800, 25, Color.Pink);
     Raylib.DrawCircle(400, 50, 50, Color.Green);
 
-    float circleX = screenWidth / 2f;
-    float circleY = screenHeight / 2f;
-    float circleRadius = 20;
-    float rectangleWidth = 40;
-    float rectangleHeight = 60;
-    float rectangleX = circleX - rectangleWidth / 2;
-    float rectangleY = circleY + circleRadius;
-    float speed = 5f;
-
-    if (Raylib.IsKeyDown(KeyboardKey.Right))
+    if (Raylib.IsKeyDown(KeyboardKey.Right) && (circleX + circleRadius + speed) < screenWidth)
     {
         circleX += speed;
     }
-    if (Raylib.IsKeyDown(KeyboardKey.Left))
+    if (Raylib.IsKeyDown(KeyboardKey.Left) && (circleX - circleRadius - speed) > 0)
     {
         circleX -= speed;
     }
-    if (Raylib.IsKeyDown(KeyboardKey.Down))
+    if (Raylib.IsKeyDown(KeyboardKey.Down) && (circleY + rectangleHeight + circleRadius + speed) < screenHeight)
     {
         circleY += speed;
     }
-    if (Raylib.IsKeyDown(KeyboardKey.Up))
+    if (Raylib.IsKeyDown(KeyboardKey.Up) && (circleY - circleRadius - speed) > 0)
     {
         circleY -= speed;
     }
 
-    rectangleX = circleX - rectangleWidth / 2;
-    rectangleY = circleY + circleRadius;
+    float rectangleX = circleX - rectangleWidth / 2;
+    float rectangleY = circleY + circleRadius;
 
     Raylib.DrawCircle((int)circleX, (int)circleY, circleRadius, Color.Yellow);
     Raylib.DrawRectangle((int)rectangleX, (int)rectangleY, (int)rectangleWidth, (int)rectangleHeight, Color.Blue);
 
-    Raylib.DrawText("Use arrow keys to move the character", 5, 775, 20, Color.Black);
+    Raylib.DrawRectangle(0, 0, screenWidth, 5, Color.Beige); // Övre kant
+    Raylib.DrawRectangle(0, 0, 5, screenHeight, Color.Beige); // Vänster kant
+    Raylib.DrawRectangle(screenWidth - 5, 0, 5, screenHeight, Color.Beige); // Höger kant
+
+    Raylib.DrawText("Use arrow keys to move the character", 5, 975, 20, Color.Black);
 
     Raylib.EndDrawing();
 }
