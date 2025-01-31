e5a2b5fc6525043e2538e02cd82d27cc1b7ef65b
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2025-01-31 14:48:02 +0100

    Day 6

--- vinterprojektet/Program.cs
+++ vinterprojektet/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System.Numerics;
 using Raylib_cs;
 
+/* 
+
+Jag ska lägga till en funktion där den gröna lampan längst fram blir röd av slump med Random.Next
+Om spelaren rör sig medan lampan är röd ska skärmen bli svart och en röd text ska visas där det står "GAME OVER"
+
+*/
+
 // Skärmdimensioner
 const int screenWidth = 800;
 const int screenHeight = 1000;
@@ -14,18 +21,20 @@ float rectangleWidth = 40;
 float rectangleHeight = 60;
 float speed = 2f;
 
-// Börja längst ner
+// Börjar längst ner
 float circleX = screenWidth / 2f;
 float circleY = screenHeight - rectangleHeight - circleRadius;
 
+// Spelets loop
 while (Raylib.WindowShouldClose() == false)
 {
+    // Ritar mappen
     Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Beige); // Sand
+    Raylib.DrawRectangle(0, 125, 800, 25, Color.Pink); // Mållinje
+    Raylib.DrawCircle(400, 50, 50, Color.Green); // Ljus
 
-    Raylib.ClearBackground(Color.Beige);
-    Raylib.DrawRectangle(0, 125, 800, 25, Color.Pink);
-    Raylib.DrawCircle(400, 50, 50, Color.Green);
-
+    // Rörelsekontroller
     if (Raylib.IsKeyDown(KeyboardKey.Right) && (circleX + circleRadius + speed) < screenWidth)
     {
         circleX += speed;
@@ -43,12 +52,15 @@ while (Raylib.WindowShouldClose() == false)
         circleY -= speed;
     }
 
+    // Ser till att rektangeln stannar under cirkeln
     float rectangleX = circleX - rectangleWidth / 2;
     float rectangleY = circleY + circleRadius;
 
+    // Ritar karaktären
     Raylib.DrawCircle((int)circleX, (int)circleY, circleRadius, Color.Yellow);
     Raylib.DrawRectangle((int)rectangleX, (int)rectangleY, (int)rectangleWidth, (int)rectangleHeight, Color.Blue);
 
+    // Ritar kanterna så att man inte kan gå utanför mappen
     Raylib.DrawRectangle(0, 0, screenWidth, 5, Color.Beige); // Övre kant
     Raylib.DrawRectangle(0, 0, 5, screenHeight, Color.Beige); // Vänster kant
     Raylib.DrawRectangle(screenWidth - 5, 0, 5, screenHeight, Color.Beige); // Höger kant
